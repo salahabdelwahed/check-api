eaa6bc9b11195c021d1c66ee7bf32460d36bab66
Your Name <Your email>
2024-02-23 15:54:28 +0100

    last update

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,24 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+// Import necessary
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const UserList = () => {
+  // Define state variable to store list of users
   const [listOfUsers, setListOfUsers] = useState([]);
 
+  // useEffect hook to fetch data when component mounts
   useEffect(() => {
+    // Fetch data from API
     const fetchData = async () => {
-      const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      // Update state with fetched data
       setListOfUsers(response.data);
     };
-
+    // Call fetchData function
     fetchData();
-
+   
   }, []);
-
+  // Render list of users
   return (
     <div>
       <h2>List of Users</h2>
       <ul>
-        {listOfUsers.map(user => (
+        {listOfUsers.map((user) => (
           <li key={user.id}>{user.name}</li>
         ))}
       </ul>
